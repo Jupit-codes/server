d9e3fba98f2a3d39dc94fd75563bc01a0b0f952d
Andre <quadhay@gmail.com>
2022-01-24 16:35:21 +0100

    Saved

--- model/users.js
+++ model/users.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema({
     username:{
-        type:'String',
-        required: [true, "Required"]
+        type:'String'
     },
     email:{
         type:'String',
