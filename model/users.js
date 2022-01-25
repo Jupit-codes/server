67854e3c20dc3d536d74b4b1db7bac6af4ccdc96
Andre <quadhay@gmail.com>
2022-01-25 17:47:12 +0100

    Saved

--- model/users.js
+++ model/users.js
@@ -18,9 +18,9 @@ const userSchema = mongoose.Schema({
         type:'String',
         required: [true, "Required"],
     },
-    naira_wallet:[{balance:'Number',address:'String'}],
-    btc_wallet:[{balance:'Number',address:'String'}],
-    usdt_wallet:[{balance:'Number',address:'String'}],
+    naira_wallet:[{balance:{type:'Number',set: function (v) { return Math.round(v);}},address:'String'}],
+    btc_wallet:[{balance:{type:'Number',set: function (v) { return Math.round(v);}},address:'String'}],
+    usdt_wallet:[{balance:{type:'Number',set: function (v) { return Math.round(v);}},address:'String'}],
     email_verification:{
         type:'Boolean',
         required:[true,"Required"]
