3030311759f0c77b9a555b02f0e74d9cf1363a5e
Andre <quadhay@gmail.com>
2022-01-25 17:47:31 +0100

    Saved

--- route/threshold.js
+++ route/threshold.js
@@ -5,6 +5,7 @@ import querystring from 'querystring';
 import random from 'random-number';
 import Usermodel from '../model/users.js';
 import Walletmodel from '../model/wallet_transactions.js'
+import { randomUUID } from 'crypto'
 
 
 
@@ -431,8 +432,14 @@ Router.post('/transfer/asset',middlewareVerify,(req,res)=>{
     const wallets_type = req.body.wallet_type;
     const auto_fee = req.body.auto_fee;
     const amount = req.body.amount;
+    const recipentAddress = req.body.recipentaddress
     
-   let User =  Usermodel.findById({_id:userid},function(err,docs){
+    // console.log(recipentAddress);
+    // res.json({
+    //     recipentAddress
+    // })
+    // return false;
+   let User =  Usermodel.findById({_id:userid},async function(err,docs){
         if(err){
             res.send({
                 "message":err.data,
@@ -442,19 +449,53 @@ Router.post('/transfer/asset',middlewareVerify,(req,res)=>{
         if(docs){
             
             if(wallets_type === "BTC"){
-                // res.send({
-                //     'message':'Done',
-                //     "details":docs.btc_wallet[0].address
-                // })
+                
                 let totalAmount  = parseFloat(auto_fee) + parseFloat(amount);
 
-                if(totalAmount > docs.btc_wallet[0].balance    ){
-                    let callback = creditWalletAddress(docs._id,docs.btc_wallet[0].address,wallets_type,auto_fee,totalAmount)
-                    console.log('callback',callback);
-                    res.json({
-                        "Message":"Notification Has Been Sent To BlockChain",
-                        "Status":true
-                    })
+                if(docs.btc_wallet[0].balance > totalAmount ){
+                    
+                    let jupitAddress = await checkJupitAddress(recipentAddress,wallets_type);
+                    
+                    if(jupitAddress[1]){
+                        if(jupitAddress[0]=== "JupitCustomer"){
+                            let InternalWallet = await InternalWalletTransfer(docs._id,recipentAddress,amount)
+                            
+                            if(InternalWallet){
+                                res.json(InternalWallet);
+                            }
+                            else{
+                                res.json('Error');
+                            }
+                            
+
+                            // if(InternalWallet[1]){
+                                
+                            // }
+                            // else{
+                            //     res.json(InternalWallet)
+                            // }
+                            
+                        
+                        }
+                        else{
+                             let WalletCallback =  await creditWalletAddress(docs._id,docs.btc_wallet[0].address,recipentAddress,wallets_type,auto_fee,amount)
+                            if(WalletCallback[1]){
+                                res.json({
+                                    "Message":WalletCallback[0],
+                                    "Status":true
+                                })
+                            }
+                            else{
+                                res.json({
+                                    "Message":WalletCallback[0],
+                                    "Status":false
+                                })
+                            }
+                        }
+                    }
+                    
+                   
+                       
                 }
                 else{
                     res.json({
@@ -468,7 +509,7 @@ Router.post('/transfer/asset',middlewareVerify,(req,res)=>{
             if(wallets_type === "USDT"){
                 let totalAmount  = parseFloat(auto_fee) + parseFloat(amount);
                 if(totalAmount > docs.usdt_wallet[0].balance ){
-                    let callback = creditWalletAddress(docs._id,docs.usdt_wallet[0].address,wallets_type,auto_fee,parseFloat(amount))
+                    let callback = creditWalletAddress(docs._id,docs.usdt_wallet[0].address,recipentAddress,wallets_type,auto_fee,parseFloat(amount))
                     res.send(callback);
                 }
                 else{
@@ -493,9 +534,10 @@ Router.post('/transfer/asset',middlewareVerify,(req,res)=>{
     
 });
 
-async function creditWalletAddress(userid,address,wallet_type,auto_fee,amount){
+async function creditWalletAddress(userid,address,recipentAddress,wallet_type,auto_fee,amount){
     let secret="";
     let apikey = "";
+    let isTrue ;
    
     if(wallet_type === "BTC"){
         secret="44bJugkgbvhzqaMiQ3inE8Hebeka";
@@ -505,7 +547,7 @@ async function creditWalletAddress(userid,address,wallet_type,auto_fee,amount){
 
     }
     else{
-        return "Invalid Wallet Type"
+        return ["Invalid Wallet Type",false]
     }
 
 
@@ -532,13 +574,14 @@ async function creditWalletAddress(userid,address,wallet_type,auto_fee,amount){
 
     // var secret="44bJugkgbvhzqaMiQ3inE8Hebeka";
     var time = Math.floor(new Date().getTime() / 1000);
-
+    var generate_order_id = generateuuID();
+    console.log('Amount',amount);
     var params = {
         "requests": [
           {
-            "order_id": "187795_"+userid,
-            "address": address,
-            "amount": "0.0001",
+            "order_id": "187795_"+generate_order_id,
+            "address": recipentAddress,
+            "amount": amount,
             "memo": "memo-"+userid,
             "user_id": userid,
             "message": "message-"+userid,
@@ -563,10 +606,8 @@ async function creditWalletAddress(userid,address,wallet_type,auto_fee,amount){
    
     const url = 'https://demo.thresh0ld.com/v1/sofa/wallets/678693/sender/transactions?'+ get_request_args
     
-   const successCallback=[];
-   const errorCallback=[];
    
-   await axios.post(url,params,{
+   let myAxios = await axios.post(url,params,{
         headers: {
             'Content-Type': 'application/json',
             // 'X-API-CODE':'4W1Pg2CeHQMS8hHGr',
@@ -577,18 +618,181 @@ async function creditWalletAddress(userid,address,wallet_type,auto_fee,amount){
    })
    .then((result)=>{
     
-        successCallback.push(result.data);
+        return([result.data,true])
+        // let jupitPromise = new Promise(function(resolve,reject){
+        //     isTrue = true;
+        //     if(isTrue){
+        //         resolve()
+        //     }
+            
+        // })
+        // jupitPromise.then(()=>{
+        //     return 'Completed';
+        // }).catch((err)=>{
+        //     return 'Uncompleted'
+        // })
+        
         
    })
    .catch((err)=>{
+
+    // return new Promise(function(resolve,reject){
+    //     resolve(err.response.data);
+    // })
     //    console.log('err',err.response.data)
-    errorCallback.push(err.response.data)
-   
+    // console.log(err.response.data)
+    return [err.response.data,false]
+    
+
+    // let jupitPromise = new Promise(function(resolve,reject){
+    //     isTrue = false;
+    //     if(isTrue){
+    //         resolve()
+    //     }
+    //     else{
+    //         reject();
+    //     }
+        
+    // })
+    // jupitPromise.then(()=>{
+    //     return 'Completed';
+    // }).catch((err)=>{
+    //     return 'Uncompleted'
+    // })
+
+    
     
    });
-   return {successCallback,errorCallback };
+//    console.log('myAxios',myAxios);
+   return myAxios;
+   
+   
+   
 }
 
+function generateuuID(){
+    return randomUUID(); 
+}
+
+async function checkJupitAddress(address,wallet_type){
+    const addrr = [];
+    let secret="";
+    let apikey="";
+    if(wallet_type === "BTC"){
+        secret="3A84eebqYqeU3HaaXMcEAip8zBRS";
+        apikey = "4PiVpdbyLJZatLBwR";
+    }
+    else if(wallet_type === "USDT"){
+
+    }
+    else{
+        return ["Invalid Wallet Type",false]
+    }
+    let rand = random(option_rand);
+    var option_rand = {
+            min: 48886
+            , max: 67889
+            , integer: true
+        }
+    function build(params, secret, t, r, postData) {
+        const p = params || [];
+        p.push(`t=${t}`, `r=${r}`);
+        if (!!postData) {
+            if (typeof postData === 'string') {
+                    p.push(postData);
+            } else {
+                    p.push(JSON.stringify(postData));
+            }
+        }
+        p.sort();
+        p.push(`secret=${secret}`);
+        return crypto.createHash('sha256').update(p.join('&')).digest('hex');
+    }
+
+    var time = Math.floor(new Date().getTime() / 1000);
+    var CHECKSUM = build(null,secret,time,rand,null);
+
+      
+    const parameters = {
+        t:time,
+        r:rand,
+    }
+ 
+    const get_request_args = querystring.stringify(parameters);
+   
+    const url = 'https://demo.thresh0ld.com/v1/sofa/wallets/194071/addresses?'+ get_request_args
+    
+   
+   let check = await axios.get(url,{
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-CODE':apikey,
+            'X-CHECKSUM':CHECKSUM,
+            'User-Agent': 'Node.js/16.7.0 (Windows 10; x64)'
+        }
+   })
+   .then((result)=>{
+        // console.log(result.data)
+        // return([result.data,true])  
+        result.data.wallet_address.forEach((d)=>{
+            addrr.push(d.address);
+        })
+        // return ([addrr,true])
+        console.log('myaddress',addrr)
+        if(addrr.includes(address)){
+            console.log(address)
+            console.log(addrr.includes(address))
+            return (['JupitCustomer',true]);
+        }
+        else{
+            console.log(address)
+            console.log(addrr.includes(address))
+            return (['Non-JupitCustomer',true]);
+        }
+        
+   })
+   .catch((err)=>{
+    
+    return [err.response.data,false]
+    
+   });
+
+   return check;
+  
+}
+async function InternalWalletTransfer(user_id,receipentAddress,amount){
+
+//    let getRequest =  await Usermodel.findOne({'btc_wallet[0].address':receipentAddress},(err,docs)=>{
+//         if(err){
+//            return [err,false]
+//         }
+//         else if(docs){
+
+//             Usermodel.findByIdAndUpdate(docs._id, { 
+//                 $push: { 
+//                         btc_wallet: {"balance":500,},
+                        
+//                     } 
+//                 }).exec();
+//                 return[docs,true]
+//         }
+//     }).clone().catch(function(err){ return [err,false]});
+
+
+        let getRequest = await Usermodel.findOneAndUpdate({'btc_wallet.address':receipentAddress},{$inc:{'btc_wallet.$.balance':amount}},null,(err)=>{
+            if(err){
+                return [err,false]
+            }
+            else{
+                return ['Updated Successfully',true]
+            }
+        
+        }).clone().catch(function(err){ return [err,false]});
+
+        return getRequest;
+    
+
+}
 
 
 function middlewareVerify(req,res,next){
