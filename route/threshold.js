e589c1082cec3291190cfa955f03fce52125209a
Andre <quadhay@gmail.com>
2022-02-14 12:46:00 +0100

    Saved

--- route/threshold.js
+++ route/threshold.js
@@ -830,7 +830,7 @@ Router.post('/check/customer/Address',middlewareVerify,async(req,res)=>{
     let jupitAddress = await checkJupitAddress(receipentAddress,wallet_type);
 
     console.log('jupit',jupitAddress)
-    res.send('Welcome')
+    res.send(jupitAddress)
 })
 
 Router.post('/transfer/asset',middlewareVerify,(req,res)=>{
