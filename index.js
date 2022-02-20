bf67171044805e0d9eca39e54b4ed198cc05e235
Andre <quadhay@gmail.com>
2022-02-20 23:04:52 +0100

    saved

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ app.use(bodyParser.json());
 //     "Origin, X-Requested-With, Content-Type, Accept,Authorization");
 //     next();
 // });
-app.use(cors({
-  "origin":"*"
-}));
-app.options('*', cors())
+app.use(cors());
+
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
+app.options('*', cors(corsOptions));
 app.use(helmet())
 app.use(morgan('combined'));
 
