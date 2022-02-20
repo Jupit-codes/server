52a85608dfaef21ec301cb17be49654df8e79b4b
Andre <quadhay@gmail.com>
2022-02-20 21:17:58 +0100

    Saved

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header("Access-Control-Allow-Methods : GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     next();
 });
