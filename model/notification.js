8be4be94155dba7c5803f808b53d9930c4896b02
Andre <quadhay@gmail.com>
2022-02-21 01:49:59 +0100

    Saved

---
+++ model/notification.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+
+const notification = mongoose.Schema({
+    type:{
+        type:'String',
+       
+    },
+    from_address:{
+        type:'String',
+    
+    }, 
+    amount:{
+        type:'String'
+    },
+    status:{
+        type:'String'
+    },
+    read_sender:{
+        type:'String'
+    },
+    read_receipent:{
+        type:'String'
+    },
+    updated: { type: Date, default: Date.now() },
+})
+
+// const User = mongoose.model('User',userSchema);
+// module.exports = User;
+
+// module.exports = mongoose.model('User',userSchema)
+export default mongoose.model('notification', notification);
