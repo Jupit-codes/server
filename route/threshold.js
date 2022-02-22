d603a53dde4939a9a46bb695565e62c532f34ec5
Andre <quadhay@gmail.com>
2022-02-22 15:22:45 +0100

    Saved

--- route/threshold.js
+++ route/threshold.js
@@ -1004,7 +1004,7 @@ Router.post('/notification/fetch',middlewareVerify,(req,res)=>{
             res.send({err});
         }
         else if(docs){
-            console.log("docs",docs)
+            console.log("docs Checks",docs)
             res.send(docs)
            
         }
