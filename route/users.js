0fe9ca8eabd5548e9514df12f5698e6203668229
Andre <quadhay@gmail.com>
2022-02-25 16:24:09 +0100

    saved

--- route/users.js
+++ route/users.js
@@ -76,11 +76,11 @@ router.post('/users/idcardverification',(req,res)=>{
             console.log('successfully uploaded the image!',data);
             IdCardVerification.findOne({userid:req.body.items.userid},function(err,docs){
                 if(err){
-                    res.status(503).send(err)
+                    res.status(400).send(err)
                 }
                 else if(docs){
                     if(docs.status === "Pending"){
-                        res.status(503).send("Your IDCard Verification Is Already In Progress")
+                        res.status(400).send("Your IDCard Verification Is Already In Progress")
                     }
                     else if(docs.status === "Rejected"){
                         IdCardVerification.create({
@@ -93,7 +93,7 @@ router.post('/users/idcardverification',(req,res)=>{
                         res.send("Verification Successfully Submitted")
                     }
                     else if(docs.status === "Resolved"){
-                        res.status(503).send("Previous Submission Has Already been Resolved")
+                        res.status(400).send("Previous Submission Has Already been Resolved")
                     }
                 }
                 else if(!docs){
