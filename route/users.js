d9aa5aac4bc0fb01f8b60b69d86c222f63115d34
Andre <quadhay@gmail.com>
2022-03-02 12:16:52 +0100

    Saved

--- route/users.js
+++ route/users.js
@@ -121,7 +121,7 @@ router.post('/login/2FA',(req,res)=>{
             else if(docs){
                 const validPassword = bcrypt.compareSync(password, docs.password);
                 if(validPassword){
-                    await TwoFactor.findOne({userid:docs._id},function(err,fa_docs){
+                    await TwoFactor.find({userid:docs._id},function(err,fa_docs){
                         if(err){
                             res.status(403).send(err)
                         }
