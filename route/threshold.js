b4c1cdc6862d7258d8a8a36aa25a89eadfa40dc3
wabloan <wabloan@wabloan.com>
2022-04-28 09:58:23 +0100

    save

--- route/threshold.js
+++ route/threshold.js
@@ -995,7 +995,7 @@ Router.post('/notification/fetch/title',middlewareVerify,(req,res)=>{
             res.send({err});
         }
         else if(docs){
-            console.log(docs)
+            // console.log(docs)
             res.send(docs)
            
         }
