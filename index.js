c05bbe9568ac8b60f2a3f6facd8f295c54b2edc0
wabloan <wabloan@wabloan.com>
2022-05-16 15:22:00 +0100

    Saved

--- index.js
+++ index.js
@@ -41,15 +41,15 @@ const MONGO_URI = 'mongodb+srv://odewumit:Ademilola@cluster0.9ymuh.mongodb.net/j
 // app.set("trust proxy",1);
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-// app.use(cors());
+app.use(cors());
 
-// const corsOptions = {
-//   origin: true,
-//   credentials: true
-// }
-// app.options('*', cors(corsOptions));
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
+app.options('*', cors(corsOptions));
 
-app.use(cors({ credentials: true, origin: true }));
+//app.use(cors({ credentials: true, origin: true }));
 app.use(helmet())
 app.use(morgan('combined'));
 
