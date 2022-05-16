8e7f04f593a54f18a3a671bdc876a832b68a4b37
wabloan <wabloan@wabloan.com>
2022-05-16 12:54:44 +0100

    Saved

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.use(Session({
   resave: true,
   
 }));
-app.use((req,res,next)=>{
-  console.log('Session Sense',req.session);
-  next();
-})
+// app.use((req,res,next)=>{
+//   console.log('Session Sense',req.session);
+//   next();
+// })
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
