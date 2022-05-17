65eb62929367fbcf86b33c81f1f6052a7ebb541c
wabloan <wabloan@wabloan.com>
2022-05-17 16:48:50 +0100

    Saved

--- model/users.js
+++ model/users.js
@@ -40,6 +40,9 @@ const userSchema = mongoose.Schema({
         type:'Boolean',
         default:false
     },
+    SessionMonitor:{
+        type:'String'
+    },
     updated: { type: Date, default: Date.now },
 })
 
