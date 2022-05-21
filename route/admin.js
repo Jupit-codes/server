1376fb4fffd1e0b63102592d96a7cb59a6d9cde2
wabloan <wabloan@wabloan.com>
2022-05-21 22:01:41 +0100

    saved

--- route/admin.js
+++ route/admin.js
@@ -386,7 +386,7 @@ router.post('/manual/wallet/credit',async (req,res)=>{
     }
     else if(req.body.modalTitle === "Naira Wallet Balance"){
         console.log('naiara',req.body.title)
-        let AddFund = await Usermodel.findOneAndUpdate({_id:req.body.userid},{$inc:{'naira_wallet.$.balance':req.body.value}}).exec();
+        let AddFund = await Usermodel.findOneAndUpdate({_id:req.body.userid},{$inc:{'naira_wallet.$.balance':parseFloat(req.body.value).toFixed(6)}}).exec();
         if(AddFund){
             res.send({
                 "message":"Wallet Successfully Updated",
