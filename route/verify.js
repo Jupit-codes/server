30c99f39523b18b6ce8f2d74662a90f00deb6fcf
wabloan <wabloan@wabloan.com>
2022-05-28 15:38:42 +0100

    Saved

--- route/verify.js
+++ route/verify.js
@@ -542,7 +542,7 @@ router.get('/get/current/rate',(req,res)=>{
 
 router.post('/purchase/coin',(req,res)=>{
     // console.log(req.body)
-    Usermodel.findOneAndUpdate({_id:req.body.userid},{$inc:{'naira_wallet.0.balance':- req.body.ngnamount,'btc_wallet.0.balance':req.body.btcamount}},(err,docs)=>{
+    Usermodel.findOneAndUpdate({_id:req.body.userid},{$inc:{'naira_wallet.0.balance':- req.body.ngnamount,'btc_wallet.0.balance':req.body.btcamount}},async (err,docs)=>{
         if(err){
             // console.log(err)
             res.status(400).send({
