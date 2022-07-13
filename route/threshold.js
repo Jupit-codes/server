cf5400a3534be574d63cc15528f9226dab7ce171
wabloan <wabloan@wabloan.com>
2022-07-13 12:25:42 +0100

    ok

--- route/threshold.js
+++ route/threshold.js
@@ -1067,7 +1067,7 @@ Router.post('/notification/fetch/title',middlewareVerify,(req,res)=>{
         else{
             console.log('Not Found')
         }
-    }).sort({date_created:1})
+    }).sort({date_created:-1})
 
 })
 
