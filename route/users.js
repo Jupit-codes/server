a17c3b939ad5b3e71482067fc8e4a068fde2408a
wabloan <wabloan@wabloan.com>
2022-10-04 23:57:32 +0100

    ok

--- route/users.js
+++ route/users.js
@@ -2280,10 +2280,10 @@ router.post('/user/getAllTransactions',middlewareVerify,(req,res)=>{
 router.post('/customer_webhook', (req,res)=>{
    
     // res.send(req.body)
-    console.log(req.body)
-   
+    // console.log(req.body)
+    res.status(200).end();
     if(req.body.event){
-        res.status(200).end();
+       
         // saveNotificationNEW(req.body);
         let Notify =  Notification.create({
                             type:"3",
