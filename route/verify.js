6d020eb41aaf04399095b91017c4180335aa3854
wabloan <wabloan@wabloan.com>
2022-12-21 23:29:31 +0100

    okay

--- route/verify.js
+++ route/verify.js
@@ -1719,15 +1719,15 @@ router.post('/client/withdrawal',(req,res)=>{
                         //     transaction_fee:req.body.charge,
                         //     status:'Transaction Completed'
                         // })
-                        fiatledger.create({
-                            userid:req.body.userid,
-                            email:req.body.email,
-                            amount:-req.body.amount,
-                            transaction_fee:req.body.charge,
-                            type:"Debit",
-                            diff_type:'transaction',
-                            status:'Transaction Completed'
-                        })
+                        // fiatledger.create({
+                        //     userid:req.body.userid,
+                        //     email:req.body.email,
+                        //     amount:-req.body.amount,
+                        //     transaction_fee:req.body.charge,
+                        //     type:"Debit",
+                        //     diff_type:'transaction',
+                        //     status:'Transaction Completed'
+                        // })
 
                        
                     
